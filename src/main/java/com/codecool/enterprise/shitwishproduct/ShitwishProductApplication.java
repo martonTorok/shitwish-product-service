980dd2b1749ffa29937799618abf187e143bf371
martonTorok <tormartonz@gmail.com>
2018-07-17 09:12:21 +0200

    basic file structure + maven dependencies added

---
+++ src/main/java/com/codecool/enterprise/shitwishproduct/ShitwishProductApplication.java
@@ -0,0 +1,12 @@
+package com.codecool.enterprise.shitwishproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShitwishProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShitwishProductApplication.class, args);
+    }
+}
---
+++ src/main/java/com/codecool/enterprise/shitwishproduct/model/Product.java
@@ -0,0 +1,81 @@
+package com.codecool.enterprise.shitwishproduct.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String userId;
+
+    private String name;
+
+    private String category;
+
+    private String imageUrl;
+
+    private String description;
+
+    private int price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
