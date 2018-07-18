b7373735f8d0063ae6344a5690abcfa6c39e9978
Viktor Matrai <viktor0matrai@gmail.com>
2018-07-18 10:56:56 +0200

    Handles JSON from the post req., adds product to db

--- src/main/java/com/codecool/enterprise/shitwishproduct/controller/ProductController.java
+++ src/main/java/com/codecool/enterprise/shitwishproduct/controller/ProductController.java
@@ -3,10 +3,7 @@ package com.codecool.enterprise.shitwishproduct.controller;
 import com.codecool.enterprise.shitwishproduct.model.Product;
 import com.codecool.enterprise.shitwishproduct.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -20,5 +17,12 @@ public class ProductController {
     @ResponseBody
     public Optional<Product> getProduct(@PathVariable("id") long id) {
         return productRepository.findById(id);
+    }
+
+    @RequestMapping(value = "/product", method = RequestMethod.POST, consumes = "application/json")
+    public Product addProduct(@RequestBody Product product){
+
+
+        return productRepository.saveAndFlush(product);
     }
 }
