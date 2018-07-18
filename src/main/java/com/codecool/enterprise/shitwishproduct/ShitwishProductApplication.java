6cff6f0da9a04af1a7cfbb28f31eac38942db7ff
Viktor Matrai <viktor0matrai@gmail.com>
2018-07-18 13:34:02 +0200

    Authorizing userId, with auth0, saves user_id from the token

--- src/main/java/com/codecool/enterprise/shitwishproduct/ShitwishProductApplication.java
+++ src/main/java/com/codecool/enterprise/shitwishproduct/ShitwishProductApplication.java
@@ -1,12 +1,19 @@
 package com.codecool.enterprise.shitwishproduct;
 
+import org.jose4j.jwk.HttpsJwks;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ShitwishProductApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ShitwishProductApplication.class, args);
+    }
+
+    @Bean
+    public HttpsJwks getKeyStore(){
+        return new HttpsJwks(System.getenv("JWKSURI"));
     }
 }
