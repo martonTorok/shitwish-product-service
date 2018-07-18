3260872b59589102258a181f155c468b2b68fae5
BalazsParducz <parducz.balazs@gmail.com>
2018-07-18 13:47:25 +0200

    merged with product adding

--- src/main/java/com/codecool/enterprise/shitwishproduct/controller/ProductController.java
+++ src/main/java/com/codecool/enterprise/shitwishproduct/controller/ProductController.java
@@ -3,17 +3,12 @@ package com.codecool.enterprise.shitwishproduct.controller;
 import com.codecool.enterprise.shitwishproduct.model.Product;
 import com.codecool.enterprise.shitwishproduct.repository.ProductRepository;
 import com.codecool.enterprise.shitwishproduct.service.Authentication;
-import com.codecool.enterprise.shitwishproduct.service.ProductService;
-import org.json.JSONObject;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.*;
+
 
 import java.util.Optional;
 
@@ -51,11 +46,11 @@ public class ProductController {
     @RequestMapping(value = "/product/{id}", method = RequestMethod.PUT)
     public ResponseEntity<Product> updateProduct(
             @PathVariable("id") long id,
-            @RequestHeader String header,
+            @RequestHeader String auth,
             @RequestBody Product product) {
 
         String expectedId = productRepository.getOne(id).getUserId();
-        String actualId = "";
+        String actualId = authentication.authorize(auth);
         if (!actualId.equals(expectedId)) {
             return new ResponseEntity<Product>( HttpStatus.UNAUTHORIZED );
         }
