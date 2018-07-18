45dcbbb31ea403447e2e99f6b478aa5042d325b3
martonTorok <tormartonz@gmail.com>
2018-07-18 11:27:47 +0200

    /products GETMapped

--- src/main/java/com/codecool/enterprise/shitwishproduct/controller/ProductController.java
+++ src/main/java/com/codecool/enterprise/shitwishproduct/controller/ProductController.java
@@ -1,8 +1,12 @@
 package com.codecool.enterprise.shitwishproduct.controller;
 
+import com.codecool.enterprise.shitwishproduct.model.Product;
 import com.codecool.enterprise.shitwishproduct.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ProductController {
@@ -10,4 +14,8 @@ public class ProductController {
     @Autowired
     ProductRepository productRepository;
 
+    @GetMapping(value = "/products", produces = "application/json")
+    public List<Product> viewAllProducts() {
+        return productRepository.findAll();
+    }
 }
